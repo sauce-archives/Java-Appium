2c4d448a300d6b3ce9dc1991c969f4482f3b54c0
ndmanvar <ndmanvar@yahoo.com>
2016-07-20 09:41:38 -0700

    adding

---
+++ Android/SampleSauceTest.java
@@ -0,0 +1,31 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import io.appium.java_client.android.AndroidDriver;
+import java.net.URL;
+
+public class SampleSauceCheckBoxTest {
+
+  public static final String USERNAME = "YOUR_USERNAME";
+  public static final String ACCESS_KEY = "YOUR_ACCESS_KEY";
+  public static final String URL = "http://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:80/wd/hub";
+
+  public static void main(String[] args) throws Exception {
+
+    DesiredCapabilities capabilities = new DesiredCapabilities();
+    capabilities.setCapability("platformName", "Android");
+    capabilities.setCapability("deviceName", "Samsung Galaxy S4 Emulator");
+    capabilities.setCapability("platformVersion", "4.4");
+    capabilities.setCapability("app", "http://saucelabs.com/example_files/ContactManager.apk");
+    capabilities.setCapability("browserName", "");
+    capabilities.setCapability("deviceOrientation", "portrait");
+    capabilities.setCapability("appiumVersion", "1.5.3");
+
+    WebDriver driver = new AndroidDriver<>(new URL("http://ondemand.saucelabs.com:80/wd/hub"), capabilities);
+
+    /**
+     * Test Actions here...
+     */
+
+    driver.quit();
+  }
+}
